1462d7f05b06cf5d115e2866ecefb8642049960c
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2015-04-07 15:40:57 -0400

    Created routes for robot commands

--- ms_roboto_lab_5/ms_roboto.py
+++ ms_roboto_lab_5/ms_roboto.py
@@ -9,6 +9,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/forward')
+def forward():
+    print url_for('forward')
+
+@app.route('/backward')
+def backward():
+    print url_for('backward')
+
+@app.route('/left')
+def left():
+    print url_for('left')
+
+@app.route('/right')
+def right():
+    print url_for('right')
+
+@app.route('/distance')
+def distance():
+    print url_for('distance')
+
+@app.route('/temperature')
+def temperature():
+    print url_for('temperature')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
--- ms_roboto_lab_5/ms_roboto_lab_5.ino
+++ ms_roboto_lab_5/ms_roboto_lab_5.ino
@@ -97,7 +97,7 @@ WIFI_PROFILE wireless_prof = {
           /* Gateway IP */ "10.136.160.1", };
 
 //TODO PUT IN COMPUTER IP
-String remote_server = "137.122.45.88"; // peer device IP address
+String remote_server = "137.122.47.224"; // peer device IP address
 
 String remote_port = "32114"; // arbitrary port
 
