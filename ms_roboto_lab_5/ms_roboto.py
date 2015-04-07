9dea82af3932a97d5600726c0b91e76626cd17d9
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2015-04-07 14:40:24 -0400

    removed venv and reinstalled flask

--- ms_roboto_lab_5/ms_roboto.py
+++ ms_roboto_lab_5/ms_roboto.py
@@ -1,6 +1,6 @@
-
-"""from venv.lib.python.sitepackages.flask import Flask, render_template, url_for"""
-import venv
+import sys
+sys.path.append("/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages")
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
